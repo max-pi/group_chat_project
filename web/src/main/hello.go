818fc88aa044f3f0696a140771057f0377ee6d5e
max-pi <maksym.p@gmail.com>
2017-02-27 15:01:17 -0500

    basic json api calls

--- web/src/main/hello.go
+++ web/src/main/hello.go
@@ -4,23 +4,103 @@ import (
     "fmt"
     "net/http"
     "log"
-    "github.com/go-sql-driver/mysql"
+    "strconv"
+    "encoding/json"
 )
 
 
+// handlers for the api calls
+
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+    fmt.Fprintf(w, "Hi there, I love %s! \n", r.URL.Path[1:])
 }
 
+func handler_status(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "looks like we are ok")
+}
+
+func handler_app_link(w http.ResponseWriter, r *http.Request) {
+    // TODO: redirect to the app store link
+    fmt.Fprintf(w, "app store link:")
+}
+
+func handler_group_all(w http.ResponseWriter, r *http.Request) {
+
+    // if r.Method != "POST" {
+    //     //http.ServeFile(res, req, "signup.html")
+    //     return
+    // }
+
+    all_groups := get_group_all()
+
+    text, err := json.Marshal(all_groups)
+
+    if (err != nil) {
+      // rip
+      return
+    }
+
+    fmt.Fprintf(w, string(text))
+}
+
+func handler_group(w http.ResponseWriter, r *http.Request) {
+  if r.Method != "POST" {
+    // only allow POST requests
+    return
+  }
+
+    r.ParseForm()
+    group_id := r.PostFormValue("group_id")
+    group_id_int, err := strconv.Atoi(group_id)
+
+    fmt.Fprintf(w, "yoyo")
+    fmt.Fprintf(w, string(group_id_int))
+
+    group := get_group_with_id(group_id_int)
+
+    text, err := json.Marshal(group)
+
+    if (err != nil) {
+      // rip
+      return
+    }
+
+    fmt.Fprintf(w, string(text))
+}
+
+
+
 func main() {
-    var ssl_path = "/var/www/group_chat_project/web/private/ssl"
+    var ssl_path = "/var/www/group_chat_project/web/private/ssl/"
     var crt = ssl_path+"2_erf.io.crt"
     var key = ssl_path+"amber_nopass.key"
 
-    http.HandleFunc("/", handler)
+
+    // handler definitions for various api calls
+    http.HandleFunc("/", handler) // default
+    http.HandleFunc("/status", handler_status)
+    http.HandleFunc("/app", handler_app_link)
+
+    // group info
+    http.HandleFunc("/group/all", handler_group_all)
+    http.HandleFunc("/group/", handler_group)
+    //
+    // // creates new group
+    // http.HandleFunc("/group/new", handler_group_new)
+    //
+    // // send message to group
+    // http.HandleFunc("/group/send", handler_group_send)
+    //
+    // // add person to group
+    // http.HandleFunc("/group/join", handler_group_join)
+    //
+    // // remove person from group
+    // http.HandleFunc("/group/kick", handler_group_kick)
+
     err := http.ListenAndServeTLS(":443", crt, key, nil)
 
     if err != nil {
         log.Fatal("ListenAndServe: ", err)
     }
+
 }
---
+++ web/src/main/mysql_connector.go
@@ -0,0 +1,120 @@
+package main
+
+import "database/sql"
+import _ "github.com/go-sql-driver/mysql"
+
+import "net/http"
+import "log"
+
+// Global sql.DB to access the database by all handlers
+var db *sql.DB
+var err error
+
+// db structures
+type Group struct {
+  Id int
+  Name string
+}
+
+type User struct {
+  Id int
+  Name string
+  FBID string
+}
+
+
+
+
+
+
+
+func homePage(res http.ResponseWriter, req *http.Request) {
+    http.ServeFile(res, req, "index.html")
+}
+
+
+// api db methods
+func get_group_all() []Group {
+
+  log.Printf("groups")
+
+  db := get_db();
+
+  // list of all the groups
+  var result []Group;
+
+  rows, errs := db.Query("select * from message_group")
+
+  if(errs != nil) {
+    // rip
+    return []Group{}
+  }
+
+  for rows.Next() {
+
+    var id int;
+    var name string;
+    var icon *string;
+
+    err := rows.Scan(&id, &name, &icon)
+    if (err != nil) {
+      // rip
+      log.Printf(err.Error())
+      return []Group{}
+    }
+
+    log.Printf(name)
+
+    result = append(result, Group{id, name})
+  }
+
+
+  return result;
+}
+
+
+func get_group_with_id(group_id int) Group {
+
+  db := get_db();
+
+  var id int;
+  var name string;
+  var icon *string;
+
+  db.QueryRow("select * from message_group where id = ?", group_id).Scan(&id, &name, &icon)
+
+  // if (err != nil) {
+  //   log.Printf(err.Error())
+  //   return Group{0, "error"}
+  // }
+
+  return Group{id, name};
+}
+
+
+// setup methods
+
+func mysql_test() {
+
+
+}
+
+func get_db() *sql.DB {
+  // Create an sql.DB and check for errors
+  db, err = sql.Open("mysql", "root:droplet2@/imapp")
+  if err != nil {
+      panic(err.Error())
+  }
+
+  // TODO: add this to the individual execution functions
+  // // sql.DB should be long lived "defer" closes it once this function ends
+  // defer db.Close()
+
+  // Test the connection to the database
+  err = db.Ping()
+  if err != nil {
+      panic(err.Error())
+  }
+
+  return db;
+}
